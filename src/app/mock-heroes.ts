d2bf4cd2400f641e4fffb2d409d932e39a06f2e3
phatnatevans <99981724+phatnatevans@users.noreply.github.com>
2022-03-21 09:49:19 -0500

    Step 2 Display a List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Wonder Woman' },
+  { id: 12, name: 'Aquaman' },
+  { id: 13, name: 'Supergirl' },
+  { id: 14, name: 'The Flash' }, 
+  { id: 15, name: 'Green Arrow' },
+  { id: 16, name: 'Catwoman' },
+  { id: 17, name: 'Black Panther' },
+  { id: 18, name: 'Thor' },
+  { id: 19, name: 'Killer Frost' },
+  { id: 20, name: 'Captain America' }
+];
