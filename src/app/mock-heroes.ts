326e11169166e4dd6d0d60149d4a751c209a9083
phatnatevans <99981724+phatnatevans@users.noreply.github.com>
2022-03-22 09:21:37 -0500

    step 3 Create a Feature Component

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Wonder Woman' },
-  { id: 12, name: 'Aquaman' },
-  { id: 13, name: 'Supergirl' },
-  { id: 14, name: 'The Flash' }, 
-  { id: 15, name: 'Green Arrow' },
-  { id: 16, name: 'Catwoman' },
-  { id: 17, name: 'Black Panther' },
-  { id: 18, name: 'Thor' },
-  { id: 19, name: 'Killer Frost' },
-  { id: 20, name: 'Captain America' }
+  { id: 1, name: 'Wonder Woman' },
+  { id: 2, name: 'Aquaman' },
+  { id: 3, name: 'Supergirl' },
+  { id: 4, name: 'The Flash' }, 
+  { id: 5, name: 'Green Arrow' },
+  { id: 6, name: 'Catwoman' },
+  { id: 7, name: 'Black Panther' },
+  { id: 8, name: 'Thor' },
+  { id: 9, name: 'Killer Frost' },
+  { id: 10, name: 'Captain America' }
 ];
